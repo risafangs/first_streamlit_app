2d5df5a7d4dabb919bd3797b7a776216a9f92d93
lisa fang <risafangs@users.noreply.github.com>
2023-02-17 20:20:47 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,10 @@ import streamlit
 
 streamlit.title('A Nice Title')
 
-streamlit.header('Look, it\'s a header')
+streamlit.header('Breakfast Menu')
+streamlit.text('🫐 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥬 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🍳 Hard-Boiled Free-Range Egg')
 
-streamlit.text('Words')
-streamlit.text('more words')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
